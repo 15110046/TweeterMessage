e7d586a1725d284ed7dc872f1e5a343b2ff1b0ec
Nguyen Chi Hieu <15110046a@gmail.com>
2019-03-09 18:47:47 +0700

    [final project]

---
+++ Tweeter/Comon/ExtensionUIViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ExtensionUIViewController.swift
+//  Tweeter
+//
+//  Created by Nguyen Hieu on 3/9/19.
+//  Copyright © 2019 com.nguyenhieu.game. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UIViewController {
+    func showAlert(withTitle title: String, withMessage message:String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default, handler: { action in
+        })
+        alert.addAction(ok)
+        DispatchQueue.main.async(execute: {
+            self.present(alert, animated: true)
+        })
+    }
+}
